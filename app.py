0b1a966ba9d212f482019e908502e2336d73b435
Filip Mularski <mularskif@gmail.com>
2020-08-30 22:41:52 +0200

    text and scrollbar

--- app.py
+++ app.py
@@ -6,4 +6,20 @@ root.title('Text Editor')
 # root.iconbitmap('icon.ico')
 root.geometry('1200x600')
 
+# create main frame
+frame = Frame(root)
+frame.pack(pady=5)
+
+# create scrollbar for text
+text_scroll = Scrollbar(frame)
+text_scroll.pack(side=RIGHT, fill=Y)
+
+# create textbox
+text = Text(frame, width=97, height=25, font=('Helvetica', 16), selectbackground='yellow', selectforeground='black',
+            undo=True, yscrollcommand=text_scroll.set)
+text.pack()
+
+# configure scrollbar
+text_scroll.config(command=text.yview)
+
 root.mainloop()
